e06d291e2cd39cd36f61b1fcd244c5dcacc31066
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2016-10-26 12:29:13 +0200

    added (failing) test case for balance

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CC=g++
 
 FILES= ParcoRepartTest.cpp ParcoRepart.cpp
 EXECUTABLE=Katanomi
-LFLAGS= -L/home/moritzl/WAVE/scai_lama/install/lib -lscai_lama -lscai_dmemo -lscai_common -lscai_hmemo -L/home/moritzl/Includes/gtest-1.7.0/ -lgtest
-CCFLAGS= --openmp --std=c++11 -I../../scai_lama/install/include/ -DSCAI_TRACE_ON -I/home/moritzl/Includes/gtest-1.7.0/include/
+LFLAGS= -L/home/moritzl/WAVE/scai_lama/install/lib -lscai_lama -lscai_dmemo -lscai_common -lscai_hmemo -lscai_utilskernel -L/home/moritzl/Includes/gtest-1.7.0/ -lgtest
+CCFLAGS= --openmp --std=c++11 -I../../scai_lama/install/include/ -I/home/moritzl/Includes/gtest-1.7.0/include/ -DSCAI_TRACE_ON  -DSCAI_ASSERT_LEVEL_ERROR -DSCAI_LOG_LEVEL_ERROR
 
 all:
 	$(CC) $(CCFLAGS) $(FILES) -o $(EXECUTABLE) $(LFLAGS)
--- ParcoRepartTest.cpp
+++ ParcoRepartTest.cpp
@@ -3,6 +3,9 @@
 #include <scai/lama/matrix/all.hpp>
 #include <scai/lama/matutils/MatrixCreator.hpp>
 
+#include <scai/dmemo/BlockDistribution.hpp>
+
+#include <scai/utilskernel/LArray.hpp>
 #include <scai/lama/Vector.hpp>
 
 #include <memory>
@@ -14,13 +17,15 @@
 typedef double ValueType;
 typedef int IndexType;
 
+using namespace scai;
+
 namespace ITI {
 
 class ParcoRepartTest : public ::testing::Test {
 
 };
 
-TEST_F(ParcoRepartTest, partitionerInterface) {
+TEST_F(ParcoRepartTest, testPartitionerInterface) {
 	IndexType nroot = 100;
 	IndexType n = nroot * nroot;
   IndexType k = 10;
@@ -40,6 +45,41 @@ TEST_F(ParcoRepartTest, partitionerInterface) {
 
   EXPECT_EQ(partition.size(), n);
 }
+
+TEST_F(ParcoRepartTest, testPartitionBalance) {
+  IndexType nroot = 100;
+  IndexType n = nroot * nroot;
+  IndexType k = 10;
+  scai::lama::CSRSparseMatrix<ValueType>a(n,n);
+  scai::lama::MatrixCreator::fillRandom(a, 0.01);
+  IndexType dim = 2;
+
+  scai::lama::DenseVector<ValueType> coordinates(dim*n, 0);
+  for (IndexType i = 0; i < nroot; i++) {
+    for (IndexType j = 0; j < nroot; j++) {
+      coordinates.setValue(2*(i*nroot + j), i);
+      coordinates.setValue(2*(i*nroot + j)+1, j);
+    }
+  }
+
+  scai::lama::DenseVector<ValueType> partition = ParcoRepart<ValueType>::partitionGraph(a, coordinates, dim,  k);
+
+  EXPECT_EQ(n, partition.size());
+  EXPECT_EQ(0, partition.min().getValue<ValueType>());
+  EXPECT_EQ(k-1, partition.max().getValue<ValueType>());
+  EXPECT_TRUE(partition.getDistribution().isReplicated());//for now
+
+  std::vector<IndexType> subsetSizes(k, 0);//probably replace with some Lama data structure later
+  scai::utilskernel::LArray<ValueType> localPartition = partition.getLocalValues();
+  for (IndexType i = 0; i < localPartition.size(); i++) {
+    ValueType partID = localPartition[i];
+    EXPECT_LE(partID, k);
+    EXPECT_GE(partID, 0);
+    subsetSizes[partID] += 1;
+  }
+
+}
+
 } //namespace
 
 int main(int argc, char **argv) {
