23b6da74c5af5f60b4be5a985ac108ec5cc54e59
tzovas <charilaos.tzovas@kit.edu>
2017-02-22 16:16:36 +0100

    fixes in matching

--- ParcoRepartTest.cpp
+++ ParcoRepartTest.cpp
@@ -1272,9 +1272,10 @@ TEST_F (ParcoRepartTest, testGetMatchingGrid_2D) {
     }
     
     { // print
-        std::cout<<"matched edges for "<< *comm << " :" << std::endl;
+        std::cout<<"matched edges for "<< *comm << " (local indices) :" << std::endl;
         for(int i=0; i<matching[0].size(); i++){
-            std::cout<< i<< ": ("<< dist->local2global(matching[0][i])<< ":" << dist->local2global(matching[1][i]) << ") # ";
+            //std::cout<< i<< ":global  ("<< dist->local2global(matching[0][i])<< ":" << dist->local2global(matching[1][i]) << ") # ";
+            std::cout<< i<< ": ("<< matching[0][i] << ":" << matching[1][i] << ") # ";
         }
     }
 }
