db19238536c885787517248d1708304f2e8e1f12
tzovas <charilaos.tzovas@kit.edu>
2017-03-20 13:26:40 +0100

    added aux.h

---
+++ AuxiliaryFunctions.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <chrono>
+
+#include "PrioQueue.h"
+#include "ParcoRepart.h"
+#include "MultiLevel.h"
+#include "HilbertCurve.h"
+#include "Settings.h"
+#include "FileIO.h"
+
+
+namespace ITI{
+
+class aux{
+public:
+    typedef int IndexType;
+    typedef double ValueType;
+    
+//------------------------------------------------------------------------------   
+    
+static void writeHeatLike_local_2D(std::vector<IndexType> input,IndexType sideLen, IndexType dim, const std::string filename){
+    std::ofstream f(filename);
+    if(f.fail())
+        throw std::runtime_error("File "+ filename+ " failed.");
+    
+    f<< "$map2 << EOD" << std::endl;
+    
+    for(IndexType i=0; i<sideLen; i++){
+        for(IndexType j=0; j<sideLen; j++){
+            //for(IndexType d=0; d<dim; d++){
+            f<< j << " " << i << " " << input[i*sideLen+j] << std::endl;
+            //PRINT( i/dim<< " " << i%dim << " " << input[i*dim +dim] );
+        }
+        f<< std::endl;
+    }
+    f<< "EOD"<< std::endl;
+    f<< "set title \"Pixeled partition for file " << filename << "\" " << std::endl;
+    f << "plot '$map2' using 2:1:3 with image" << std::endl;
+}    
+//------------------------------------------------------------------------------
+
+static void print2DGrid(scai::lama::CSRSparseMatrix<ValueType>& adjM, scai::lama::DenseVector<IndexType>& partition  ){
+    
+    IndexType N= adjM.getNumRows();
+    
+    IndexType numX = std::sqrt(N);
+    IndexType numY = numX;
+    SCAI_ASSERT_EQ_ERROR(N , numX*numY, "Input not a grid" );
+    
+    if( numX>65 ){
+        PRINT("grid too big to print, aborting.");
+        return;
+    }
+        
+    //get the border nodes
+    scai::lama::DenseVector<IndexType> border(adjM.getColDistributionPtr(), 0);
+    border = ParcoRepart<IndexType,ValueType>::getBorderNodes( adjM , partition);
+    
+    IndexType partViz[numX][numY];   
+    IndexType bordViz[numX][numY]; 
+    for(int i=0; i<numX; i++)
+        for(int j=0; j<numY; j++){
+            partViz[i][j]=partition.getValue(i*numX+j).getValue<IndexType>();
+            bordViz[i][j]=border.getValue(i*numX+j).getValue<IndexType>();
+        }
+
+    scai::dmemo::CommunicatorPtr comm = scai::dmemo::Communicator::getCommunicatorPtr();
+    comm->synchronize();
+
+    if(comm->getRank()==0 ){            
+        std::cout<<"----------------------------"<< " Partition  "<< *comm << std::endl;    
+        for(int i=0; i<numX; i++){
+            for(int j=0; j<numY; j++){
+                if(bordViz[i][j]==1) 
+                    std::cout<< "\033[1;31m"<< partViz[i][j] << "\033[0m" <<"-";
+                else
+                    std::cout<< partViz[i][j]<<"-";
+            }
+            std::cout<< std::endl;
+        }
+    }
+
+}
+//------------------------------------------------------------------------------
+   
+}; //class aux
+}// namespace ITI
---
+++ auxTest.cpp
@@ -0,0 +1,243 @@
+#include <scai/lama.hpp>
+
+#include <scai/lama/matrix/all.hpp>
+#include <scai/lama/matutils/MatrixCreator.hpp>
+
+#include <scai/dmemo/BlockDistribution.hpp>
+#include <scai/dmemo/Distribution.hpp>
+
+#include <scai/hmemo/Context.hpp>
+#include <scai/hmemo/HArray.hpp>
+
+#include <scai/utilskernel/LArray.hpp>
+#include <scai/lama/Vector.hpp>
+
+#include <algorithm>
+#include <memory>
+#include <cstdlib>
+#include <numeric>
+
+#include "MeshGenerator.h"
+#include "FileIO.h"
+#include "MultiLevel.h"
+#include "ParcoRepart.h"
+#include "LocalRefinement.h"
+#include "SpectralPartition.h"
+#include "gtest/gtest.h"
+
+#include <boost/filesystem.hpp>
+
+#include "AuxiliaryFunctions.h"
+
+typedef double ValueType;
+typedef int IndexType;
+
+using namespace scai;
+
+namespace ITI {
+
+class auxTest : public ::testing::Test {
+
+};
+
+
+TEST_F (auxTest, testMultiLevelStep_dist) {
+
+    const IndexType N = 60;
+    
+    scai::dmemo::CommunicatorPtr comm = scai::dmemo::Communicator::getCommunicatorPtr();
+    scai::dmemo::DistributionPtr distPtr ( scai::dmemo::Distribution::getDistributionPtr( "BLOCK", comm, N) );
+    scai::dmemo::DistributionPtr noDistPtr(new scai::dmemo::NoDistribution( N ));
+    const IndexType k = comm->getSize();
+    
+    scai::lama::CSRSparseMatrix<ValueType> graph;
+    
+    // create random graph
+    scai::common::scoped_array<ValueType> adjArray( new ValueType[ N*N ] );
+    
+    //initialize matrix with zeros
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++)
+            adjArray[i*N+j]=0;
+        
+    srand(time(NULL));
+    IndexType numEdges = int (1.12*N);
+    for(IndexType i=0; i<numEdges; i++){
+        // a random position in the matrix
+        IndexType x = rand()%N;
+        IndexType y = rand()%N;
+        adjArray[ x+y*N ]= 1;
+        adjArray[ x*N+y ]= 1;
+    }
+    graph.setRawDenseData( N, N, adjArray.get() );
+    EXPECT_TRUE( graph.isConsistent() );
+    EXPECT_TRUE( graph.checkSymmetry() );
+    ValueType beforel1Norm = graph.l1Norm().Scalar::getValue<ValueType>();
+    IndexType beforeNumValues = graph.getNumValues();
+    graph.redistribute( distPtr , noDistPtr);
+    
+    // node weights = 1
+    DenseVector<IndexType> uniformWeights = DenseVector<IndexType>(graph.getRowDistributionPtr(), 1);
+    //ValueType beforeSumWeigths = uniformWeights.l1Norm().Scalar::getValue<ValueType>();
+    IndexType beforeSumWeigths = N;
+    //uniformWeights.redistribute( distPtr );
+    
+    //coordinates at random and redistribute
+    std::vector<DenseVector<ValueType>> coords(2);
+    for(IndexType i=0; i<2; i++){ 
+	coords[i].allocate(N);
+	coords[i] = static_cast<ValueType>( 0 );
+        // set random coordinates
+        for(IndexType j=0; j<N; j++){
+            coords[i].setValue(j, rand()%10);
+        }
+        coords[i].redistribute( distPtr );
+    }
+    
+    struct Settings Settings;
+    Settings.numBlocks= k;
+    Settings.epsilon = 0.2;
+    Settings.multiLevelRounds= 3;
+    Settings.coarseningStepsBetweenRefinement = 3;
+    Settings.useGeometricTieBreaking = true;
+    Settings.dimensions= 2;
+    
+    DenseVector<IndexType> partition= ParcoRepart<IndexType, ValueType>::initialPartition(graph, coords, Settings);
+    //partition.redistribute( distPtr );
+    
+    ITI::MultiLevel<IndexType, ValueType>::multiLevelStep(graph, partition, uniformWeights, coords, Settings);
+    //ITI::aux::multiLevelStep(graph, partition, uniformWeights, coords, Settings);
+    
+    EXPECT_EQ( graph.l1Norm() , beforel1Norm);
+    EXPECT_EQ( graph.getNumValues() , beforeNumValues);
+    DenseVector<ValueType> newWeights(N,0);
+    assert(uniformWeights.size()==N);
+    for(int i=0; i<N; i++){
+        newWeights.setValue(i, uniformWeights.getValue(i) );
+    }
+    //PRINT(newWeights.l1Norm() );
+    EXPECT_EQ( newWeights.l1Norm() , beforeSumWeigths );
+    
+}
+//-------------------------------------------------------------------------
+
+TEST_F (auxTest, testInitialPartitions){
+
+    std::string path = "meshes/bigtrace/";
+    std::string fileName = "bigtrace-00021.graph";
+    std::string file = path + fileName;
+    std::ifstream f(file);
+    IndexType dimensions= 2;
+    IndexType N, edges;
+    f >> N >> edges; 
+    
+    std::cout<< "node= "<< N << std::endl;
+    scai::dmemo::CommunicatorPtr comm = scai::dmemo::Communicator::getCommunicatorPtr();
+    // for now local refinement requires k = P
+    IndexType k = comm->getSize();
+    //
+    
+    std::string destPath = "./partResults/"+fileName+"/blocks_"+std::to_string(k)+"/";
+    boost::filesystem::create_directories( destPath );    
+    /*
+    if( !boost::filesystem::create_directory( destPath ) ){
+        throw std::runtime_error("Directory "+ destPath + " could not be created");
+    }
+    */
+    scai::dmemo::DistributionPtr dist ( scai::dmemo::Distribution::getDistributionPtr( "BLOCK", comm, N) );  
+    scai::dmemo::DistributionPtr noDistPointer(new scai::dmemo::NoDistribution(N));
+    CSRSparseMatrix<ValueType> graph = FileIO<IndexType, ValueType>::readGraph(file );
+    graph.redistribute(dist, noDistPointer);
+    
+    std::vector<DenseVector<ValueType>> coordinates = FileIO<IndexType, ValueType>::readCoords( std::string(file + ".xyz"), N, dimensions);
+    EXPECT_TRUE(coordinates[0].getDistributionPtr()->isEqual(*dist));
+    
+    EXPECT_EQ( graph.getNumColumns(), graph.getNumRows());
+    EXPECT_EQ(edges, (graph.getNumValues())/2 );   
+    
+    struct Settings settings;
+    settings.numBlocks= k;
+    settings.epsilon = 0.1;
+    settings.dimensions = dimensions;
+    settings.pixeledDetailLevel = 6;        // for a 16x16 coarsen graph in 2D, 16x16x16 in 3D
+    settings.useGeometricTieBreaking =1;
+    settings.fileName = fileName;
+    
+    ValueType cut;
+    ValueType imbalance;
+    
+    std::string logFile = destPath + "resutls.log";
+    std::ofstream logF(logFile);
+    logF<< "Results for file " << fileName << std::endl;
+    logF<< "node= "<< N << " , edges= "<< edges << " , blocks= "<< k<< std::endl<< std::endl;
+    settings.print( logF );
+    if( comm->getRank()==0)    settings.print( std::cout );
+    logF<< std::endl<< std::endl << "Only initial partition, no MultiLevel or LocalRefinement"<< std::endl << std::endl;
+    
+    //------------------------------------------- pixeled
+    
+    if(comm->getRank()==0) std::cout <<std::endl<<std::endl;
+    PRINT0("Get a pixeled partition");
+    // get a pixeledPartition
+    scai::lama::DenseVector<IndexType> pixeledPartition = ParcoRepart<IndexType, ValueType>::pixelPartition( graph, coordinates, settings);
+    
+    //aux::print2DGrid( graph, pixeledPartition );
+    if(dimensions==2){
+        ITI::FileIO<IndexType, ValueType>::writeCoordsDistributed_2D( coordinates, N, destPath+"pixelPart");
+    }
+    //cut = comm->getSize() == 1 ? computeCut(input, result) : comm->sum(localSumOutgoingEdges(input, false)) / 2;
+    cut = ParcoRepart<IndexType, ValueType>::computeCut( graph, pixeledPartition);
+    imbalance = ParcoRepart<IndexType, ValueType>::computeImbalance( pixeledPartition, k);
+    logF<< "-- Pixeled partition " << std::endl;
+    logF<< "\tcut: " << cut << " , imbalance= "<< imbalance;
+    logF  << std::endl  << std::endl; 
+    
+    //------------------------------------------- hilbert/sfc
+    
+    // the partitioning may redistribute the input graph
+    graph.redistribute(dist, noDistPointer);
+    for(int d=0; d<dimensions; d++){
+        coordinates[d].redistribute( dist );
+    }    
+    if(comm->getRank()==0) std::cout <<std::endl<<std::endl;
+    PRINT0( "Get a hilbert/sfc partition");
+    // get a hilbertPartition
+    scai::lama::DenseVector<IndexType> hilbertPartition = ParcoRepart<IndexType, ValueType>::initialPartition( graph, coordinates, settings);
+    
+    //aux::print2DGrid( graph, hilbertPartition );
+    if(dimensions==2){
+        ITI::FileIO<IndexType, ValueType>::writeCoordsDistributed_2D( coordinates, N, destPath+"hilbertPart");
+    }
+    cut = ParcoRepart<IndexType, ValueType>::computeCut( graph, hilbertPartition);
+    imbalance = ParcoRepart<IndexType, ValueType>::computeImbalance( hilbertPartition, k);
+    logF<< "-- Hilbert/sfc partition " << std::endl;
+    logF<< "\tcut: " << cut << " , imbalance= "<< imbalance;
+    logF  << std::endl  << std::endl;
+
+    //------------------------------------------- spectral
+    
+    // the partitioning may redistribute the input graph
+    graph.redistribute(dist, noDistPointer);
+    for(int d=0; d<dimensions; d++){
+        coordinates[d].redistribute( dist );
+    }
+    if(comm->getRank()==0) std::cout <<std::endl<<std::endl;
+    PRINT0("Get a spectral partition");
+    // get spectral partition
+    scai::lama::DenseVector<IndexType> spectralPartition = SpectralPartition<IndexType, ValueType>::getPartition( graph, coordinates, settings);
+    
+    //aux::print2DGrid( graph, spectralPartition );
+    if(dimensions==2){
+        ITI::FileIO<IndexType, ValueType>::writeCoordsDistributed_2D( coordinates, N, destPath+"spectralPart");
+    }
+    cut = ParcoRepart<IndexType, ValueType>::computeCut( graph, spectralPartition);
+    imbalance = ParcoRepart<IndexType, ValueType>::computeImbalance( spectralPartition, k);
+    logF<< "-- Spectral partition " << std::endl;
+    logF<< "\tcut: " << cut << " , imbalance= "<< imbalance;
+    
+    if(comm->getRank()==0){
+        std::cout<< "Output files written in " << destPath << std::endl;
+    }
+}
+
+}
