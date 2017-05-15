5807e5b3dc95e0d2d671856b71bf06a64af7c2ed
tzovas <charilaos.tzovas@kit.edu>
2017-05-15 19:57:16 +0200

    started 1D partition in RecursiveBisection

--- RecursiveBisectionTest.cpp
+++ RecursiveBisectionTest.cpp
@@ -36,6 +36,51 @@ class RecursiveBisectionTest : public ::testing::Test {
 
 };
 
+
+TEST_F(RecursiveBisectionTest, testPrefixSum){
+ 
+    scai::dmemo::CommunicatorPtr comm = scai::dmemo::Communicator::getCommunicatorPtr();
+    IndexType sideLen= comm->getSize()*2;
+    int dim = 2;
+    IndexType N= std::pow( sideLen, dim );   // for a N^dim grid
+    scai::dmemo::DistributionPtr blockDist ( scai::dmemo::Distribution::getDistributionPtr( "BLOCK", comm, N) );
+    scai::lama::DenseVector<ValueType> nodeWeights( blockDist );
+    IndexType localN = nodeWeights.getDistributionPtr()->getLocalSize();
+    
+    //create local (random) weights
+    {
+        scai::hmemo::WriteAccess<ValueType> localPart(nodeWeights.getLocalValues());
+        srand(time(NULL));
+        for(int i=0; i<localN; i++){
+            //localPart[i] = rand()%10+2;
+            localPart[i] = i;
+        }
+    }
+    IndexType k1= std::pow(comm->getSize(), 0.5);
+    IndexType dimensionToPartition = 0;
+    Settings settings;
+    settings.dimensions = dim;
+    
+    scai::lama::DenseVector<ValueType> part1D = RecursiveBisection<IndexType, ValueType>::partition1D( nodeWeights, k1, dimensionToPartition, sideLen, settings);
+    
+    if(comm->getRank() ==0){
+        for(int i=0; i<part1D.getLocalValues().size(); i++){
+            std::cout<< *comm <<": "<< part1D.getLocalValues()[i] << std::endl;
+        }
+    }
+    
+    // TODO: add proper tests
+    /*
+    std::vector<ValueType> part1DWeights(k1,0);
+    for(int h=0; h<part1D.size(); h++){
+        for(int i=0; i<localN; i++){
+            
+        }
+    }
+    */
+}
+//---------------------------------------------------------------------------------------
+
 TEST_F(RecursiveBisectionTest, testIndexTo){
     
     IndexType sideLen=4;
