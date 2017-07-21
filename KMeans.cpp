fc303921c5c6b49f66fa888246ba8a712b4b4a3b
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2017-07-21 12:03:31 +0200

    added rebalancing method to Kmeans

--- KMeans.cpp
+++ KMeans.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <set>
+#include <cmath>
 
 #include <scai/dmemo/NoDistribution.hpp>
 
@@ -13,6 +14,8 @@
 
 namespace ITI {
 namespace KMeans {
+
+using scai::lama::Scalar;
 
 template<typename IndexType, typename ValueType>
 std::vector<std::vector<ValueType> > findInitialCenters(
@@ -122,8 +125,14 @@ DenseVector<IndexType> assignBlocks(
 	const IndexType dim = coordinates.size();
 	const IndexType n = nodeWeights.size();
 	const scai::dmemo::DistributionPtr dist = coordinates[0].getDistributionPtr();
+	const scai::dmemo::CommunicatorPtr comm = dist->getCommunicatorPtr();
 	const IndexType localN = nodeWeights.getLocalValues().size();
+	const IndexType totalWeightSum = nodeWeights.sum().Scalar::getValue<IndexType>();
 	const IndexType k = targetBlockSizes.size();
+
+	const IndexType maxIter = 20;
+
+	const IndexType optSize = std::ceil(totalWeightSum / k );
 
 	//prepare data structure for squared distances
 	std::vector<std::vector<ValueType>> squaredDistances(k);
@@ -146,15 +155,21 @@ DenseVector<IndexType> assignBlocks(
 	}
 
 	//compute assignment and balance
+	std::vector<ValueType> influence(k, 1);
 	DenseVector<IndexType> assignment(dist, 0);
-	std::vector<IndexType> blockWeights(k,0);
+
+	ValueType imbalance;
+	IndexType iter = 0;
+	//iterate if necessary to achieve balance
+	do
 	{
+		std::vector<IndexType> blockWeights(k,0);
 		scai::hmemo::ReadAccess<IndexType> rWeights(nodeWeights.getLocalValues());
 		scai::hmemo::WriteAccess<IndexType> wAssignment(assignment.getLocalValues());
 		for (IndexType i = 0; i < localN; i++) {
 			int bestBlock = 0;
 			for (IndexType j = 0; j < k; j++) {
-				if (squaredDistances[j][i] < squaredDistances[bestBlock][i]) {
+				if (squaredDistances[j][i]/influence[j] < squaredDistances[bestBlock][i]/influence[bestBlock]) {
 					bestBlock = j;
 				}
 			}
@@ -162,9 +177,25 @@ DenseVector<IndexType> assignBlocks(
 			blockWeights[bestBlock] += rWeights[i];
 		}
 
+		std::vector<IndexType> totalWeight(k, 0);
+		for (IndexType j = 0; j < k; j++) {
+			totalWeight[j] = comm->sum(blockWeights[j]);
+			influence[j] = std::min(influence[j] * std::sqrt(ValueType(optSize) / totalWeight[j]), ValueType(20));
 
-		//iterate if necessary to achieve balance
-	}
+			if (comm->getRank() == 0) {
+				std::cout << "Iter " << iter << ", block " << j << " has size " << totalWeight[j] << ", setting influence to ";
+				std::cout << influence[j];
+				std::cout << std::endl;
+			}
+		}
+
+		IndexType maxBlockWeight = *std::max_element(totalWeight.begin(), totalWeight.end());
+		imbalance = (ValueType(maxBlockWeight - optSize)/ optSize);
+		iter++;
+
+		if (comm->getRank() == 0) std::cout << "Iter " << iter << ", imbalance : " << imbalance << std::endl;
+	} while (imbalance > epsilon && iter < maxIter);
+
 
 	return assignment;
 }
