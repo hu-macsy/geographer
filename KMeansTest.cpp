e25829ea00a242896620311ddfb955a2a2daeb22
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2017-07-21 16:02:31 +0200

    added unit tests for KMeans

---
+++ KMeansTest.cpp
@@ -0,0 +1,100 @@
+#include "FileIO.h"
+
+#include "KMeans.h"
+
+#include "gtest/gtest.h"
+
+typedef double ValueType;
+typedef int IndexType;
+
+namespace ITI {
+
+using scai::lama::CSRSparseMatrix;
+using scai::lama::DenseVector;
+
+class KMeansTest : public ::testing::Test {
+
+};
+
+TEST_F(KMeansTest, testFindInitialCenters) {
+	std::string path = "meshes/bubbles/";
+	std::string fileName = "bubbles-00010.graph";
+	std::string graphFile = path + fileName;
+	std::string coordFile = graphFile + ".xyz";
+	const IndexType dimensions = 2;
+	const IndexType k = 8;
+
+	CSRSparseMatrix<ValueType> graph = FileIO<IndexType, ValueType>::readGraph(graphFile );
+	const IndexType n = graph.getNumRows();
+	std::vector<DenseVector<ValueType>> coords = FileIO<IndexType, ValueType>::readCoords( std::string(coordFile), n, dimensions);
+	DenseVector<IndexType> uniformWeights = DenseVector<IndexType>(graph.getRowDistributionPtr(), 1);
+
+	std::vector<std::vector<ValueType> > centers = KMeans::findInitialCenters(coords, k, uniformWeights);
+
+	//check for size
+	EXPECT_EQ(dimensions, centers.size());
+	EXPECT_EQ(k, centers[0].size());
+
+	bool allDistinct = true;
+	//check for distinctness
+	for (IndexType i = 0; i < k; i++) {
+		for (IndexType j = i+1; j < k; j++) {
+			bool differenceFound = false;
+			for (IndexType d = 0; d < dimensions; d++) {
+				if (centers[d][i] != centers[d][j]) {
+					differenceFound = true;
+				}
+			}
+			if (!differenceFound) {
+				allDistinct = false;
+				std::cout << "Centers " << i << " and " << j << " are both at ";
+				for (IndexType d = 0; d < dimensions; d++) {
+					std::cout << "(" << centers[d][i] << "|" << centers[d][j] << ") ";
+				}
+				std::cout << std::endl;
+			}
+		}
+	}
+	EXPECT_TRUE(allDistinct);
+}
+
+TEST_F(KMeansTest, testFindCenters) {
+	std::string path = "meshes/bubbles/";
+	std::string fileName = "bubbles-00010.graph";
+	std::string graphFile = path + fileName;
+	std::string coordFile = graphFile + ".xyz";
+	const IndexType dimensions = 2;
+
+	//load graph and coords
+	CSRSparseMatrix<ValueType> graph = FileIO<IndexType, ValueType>::readGraph(graphFile );
+	const scai::dmemo::DistributionPtr dist = graph.getRowDistributionPtr();
+	const scai::dmemo::CommunicatorPtr comm = dist->getCommunicatorPtr();
+	const IndexType n = graph.getNumRows();
+	std::vector<DenseVector<ValueType>> coords = FileIO<IndexType, ValueType>::readCoords( std::string(coordFile), n, dimensions);
+
+	const IndexType k = comm->getSize();
+
+	//create random partition
+	DenseVector<IndexType> randomValues;
+	randomValues.setRandom(dist, 1);
+	randomValues.sort(true);
+	DenseVector<IndexType> part = DenseVector<IndexType>(randomValues.getDistributionPtr(), comm->getRank());
+	part.redistribute(dist);
+
+	DenseVector<IndexType> uniformWeights = DenseVector<IndexType>(graph.getRowDistributionPtr(), 1);
+
+	//get centers
+	std::vector<std::vector<ValueType> > centers = KMeans::findCenters(coords, part, k,	uniformWeights);
+
+	//check for size
+	EXPECT_EQ(dimensions, centers.size());
+	EXPECT_EQ(k, centers[0].size());
+
+	//create invalid partition
+	part = DenseVector<IndexType>(dist, 0);
+
+	//get centers
+	centers = KMeans::findCenters(coords, part, k,	uniformWeights);
+}
+
+}
