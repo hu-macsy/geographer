9fe06f22d38d5a23f1bac50fb15c2b47b6b94683
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2018-03-28 15:36:20 +0200

    fixed possible bug in repartitioning with k-means

--- KMeans.h
+++ KMeans.h
@@ -206,7 +206,7 @@ DenseVector<IndexType> computePartition(const std::vector<DenseVector<ValueType>
 		if (iter < samplingRounds) {
 			lastIndex = firstIndex + samples[iter];
 			std::sort(firstIndex, lastIndex);//sorting not really necessary, but increases locality
-			ValueType ratio = ValueType(samples[iter]) / localN;
+			ValueType ratio = ValueType(samples[iter]*p) / globalN;
 			for (IndexType j = 0; j < k; j++) {
 				adjustedBlockSizes[j] = ValueType(blockSizes[j]) * ratio;
 			}
