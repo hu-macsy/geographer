349b47bd47aeb0afe574d5d4ae11e4ac07a60fbc
tzovas <tzovas.charilaos@hu-berlin.de>
2019-06-25 18:42:52 +0200

    text styling in parseArgs

--- src/parseArgs.cpp
+++ src/parseArgs.cpp
@@ -56,9 +56,8 @@ Options populateOptions() {
     ("maxKMeansIterations", "Tuning parameter for K-Means", value<IndexType>())
     ("tightenBounds", "Tuning parameter for K-Means")
     ("erodeInfluence", "Tuning parameter for K-Means, in case of large deltas and imbalances.")
-    ("hierLevels", "The number of blocks per level. Total number of PEs (=number of leaves) is \
-					the product for all hierLevels[i] and there are hierLevels.size() hierarchy levels. Example: --hierLevels 3 4 10, there are 3 levels. \
-					In the first one, each node has 3 children, in the next one each node has 4 and in the last, each node has 10. In total 3*4*10= 120 leaves/PEs", value<std::string>())
+    // using '/' to seperate the lines breaks the output message
+    ("hierLevels", "The number of blocks per level. Total number of PEs (=number of leaves) is the product for all hierLevels[i] and there are hierLevels.size() hierarchy levels. Example: --hierLevels 3 4 10, there are 3 levels. In the first one, each node has 3 children, in the next one each node has 4 and in the last, each node has 10. In total 3*4*10= 120 leaves/PEs", value<std::string>())
     //output
     ("outFile", "write result partition into file", value<std::string>())
     //debug
