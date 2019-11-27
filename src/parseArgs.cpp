9f733adc07a609eb5b4b9f39f522c755833744c6
tzovas <tzovas.charilaos@hu-berlin.de>
2019-11-27 18:18:24 +0100

    Dev: argument to store info

--- src/parseArgs.cpp
+++ src/parseArgs.cpp
@@ -186,7 +186,7 @@ Settings interpretSettings(cxxopts::ParseResult vm) {
         }
         settings.storeInfo = true;
     }
-    if( settings.storeInfo && settings.outFile=="-" ) {
+    if( settings.storeInfo && (settings.outFile=="-" or settings.outDir=="-") ) {
         if(comm->getRank()==0){
             std::cout << "Option to store information used but no output file given to write to. Specify an output file name using the option --outFile. Aborting." << std::endl;
         }
